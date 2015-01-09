e2ed4a6ca6f5337b8f2a2f1b31d3ae4acf2c1405
bs2110055 <bsiebert@student.rcc.edu>
2015-01-08 20:30:24 -0800

    Signed-off-by: bs2110055 <bsiebert@student.rcc.edu>

---
+++ Hmwk/Assignment 2/Savitch_9thEd_Chap2_PracProg1/main.cpp
@@ -0,0 +1,42 @@
+/* 
+   File:   main.cpp
+   Author: Brian Siebert
+   Created on January 8, 2015, 7:41 PM
+   Purpose: Hmwk, Savitch_9thEd_Chap2_PracProg1
+ */
+
+//System Libraries
+#include <iostream>
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution Begins Here!
+int main(int argc, char** argv) {
+    float x, ounce, ton, boxes;
+      
+    cout << "This program will read the weight of a package of\n";
+    cout << "breakfast cereal in ounces and output the weight\n";  
+    cout << "in metric tons as well as the number of boxes\n";    
+    cout << "needed to yield 1 metric ton of cereal.\n\n";     
+    
+    cout << "Press return after entering a number.\n";
+    cout << "Enter the weight of a package of breakfast cereal in ounces:\n";
+    
+    cin >> ounce;
+    ton = ounce/35273.92;
+    boxes = 35273.92/ounce;
+    cout << "Weight in metic tons:\n";
+    cout << ton;
+    cout << "\n";
+    cout << "Number of boxes needed to yield 1 metric ton of cereal:\n";
+    cout << boxes;
+    
+    return 0;
+}
+
+
