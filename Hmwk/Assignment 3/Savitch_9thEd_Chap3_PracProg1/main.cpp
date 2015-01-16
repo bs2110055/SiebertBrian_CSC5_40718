a41f8b64f6f3e3b5744361618cbd96a6b21f6653
bs2110055 <bsiebert@student.rcc.edu>
2015-01-15 21:45:19 -0800

    Signed-off-by: bs2110055 <bsiebert@student.rcc.edu>

---
+++ Hmwk/Assignment 3/Savitch_9thEd_Chap3_PracProg1/main.cpp
@@ -0,0 +1,41 @@
+/* 
+   File:   main.cpp
+   Author: Brian Siebert
+   Created on January 15, 2015, 8:31 PM
+   Purpose: Hmwk, Savitch_9thEd_Chap3_PracProg1
+ */
+//System Libraries
+#include <iostream>
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution Begins Here!
+int main(int argc, char** argv) {
+    
+    char p1;//Player one's choice.
+    char p2;//Player two's choice.
+    
+    cout<<"This program will score a game of paper-rock-scissors."
+    cout<<endl;
+    cout<<"Please select 'P' for Paper, 'R' for Rock, or 'S' for scissors."endl;
+    cout<<"Player one, please enter your selection: ";
+    cin>>p1;
+    cout<<endl;      
+    cout<<endl; 
+    cout<<endl; 
+    cout<<endl; 
+    cout<<endl; 
+    cout<<endl; 
+    cout<<endl; 
+    cout<<endl; 
+    cout<<endl; 
+    cout<<endl; 
+    cout<<"Please select 'P' for Paper, 'R' for Rock, or 'S' for scissors."endl;
+    cout<<"Player two, please enter your selection: ";
+    cin>>p2;
+    
